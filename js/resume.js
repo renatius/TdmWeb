e8ad6fda75b5756128e7bf90379caf6198418abf
Renato Mancuso <mancuso@renatomancuso.com>
2014-06-22 12:23:01 +0200

    New version of resume

--- js/resume.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#page').removeClass('border-radius');
-	$('#contact_info').append('<div class="email">e-mail: <a class="value" href="mailto:mancuso@renatomancuso.com">mancuso@renatomancuso.com</a></div>');
-    /*
-    var current_year = new Date().getFullYear();
-	var experience = current_year - 1997;
-	$('#experience').html(experience.toString());
-	*/
-});
